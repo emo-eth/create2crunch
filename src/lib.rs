b286a4d7077ba68ddad1661a5a98ec765c6455bf
emo.eth <emodoteth@gmail.com>
2025-03-13 13:38:51 -0700

    default work size

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,7 @@ pub mod optimize;
 mod differential_test;
 
 // Default work size
-pub const WORK_SIZE: u64 = 536_870_912; // 2^29
+pub const WORK_SIZE: u64 = 268435456; // 2^28
 
 const WORK_FACTOR: u128 = (WORK_SIZE as u128) / 1_000_000;
 const CONTROL_CHARACTER: u8 = 0xff;
